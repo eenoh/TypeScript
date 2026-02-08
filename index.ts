d24982ea936d80ff8db935337f6a8aef9c1be7b3
Emmanuel Enoh <emienoh1405@gmail.com>
2026-02-08 15:12:39 +0100

    Any Type

--- index.ts
+++ index.ts
@@ -33,11 +33,22 @@ console.log(tsEasy);
 /**
  * Type Inference
  */
-
 let tech = "TypeScript";
 let number = 8;
 let tsHard = true;
 
 console.log(typeof tech, typeof number, typeof tsHard);
 console.log(tech, number, tsHard);
+
+/**
+ * Any Type
+ */
+let color: any = "black";
+color = 20;
+color = true;
+
+// color is not a function. In development the error might not appear but at runtime
+color();
+color.toUpperCase();
+console.log(color);
 
