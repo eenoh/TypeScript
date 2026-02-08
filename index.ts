47936ddf65e0edb3a6c0cd11b7a8ba4fa79b1016
Emmanuel Enoh <emienoh1405@gmail.com>
2026-02-08 14:36:20 +0100

    Annotations

---
+++ index.ts
@@ -0,0 +1,31 @@
+type Person = {
+  name: string;
+};
+
+const person: Person = {
+  name: "Emmanuel",
+};
+
+console.log(person.name);
+
+
+
+/**
+ * Annotations
+ * Syntax: let myVar: type = value
+ */
+
+// String
+let myName: string = "Emmanuel Enoh";
+myName = "Besong Enoh";
+console.log(myName);
+
+// Number
+let age: number = 22;
+age += 2;
+console.log(age);
+
+// Boolean
+let tsEasy: boolean = false;
+console.log(tsEasy); 
+
